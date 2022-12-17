15774df1c217fcb3e9afc326bdbb14d85c381c46
srichard3 <sricha36@lion.lmu.edu>
2022-12-17 11:42:08 -0800

    Initial Commit

---
+++ KBApp/KBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KBAppApp.swift
+//  KBApp
+//
+//  Created by Sam Richard on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct KBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
